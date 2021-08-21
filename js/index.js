4cc213a54ed916d8ab504afcd4faee160b6fdde3
shadhinmonzur <shekh15-9027@diu.edu.bd>
2021-08-21 11:07:44 +0600

    function reduced

--- js/index.js
+++ js/index.js
@@ -1,39 +1,7 @@
-// function for memory 
-function updateExtraMeomry(memoryCostId,cost) {
-    const memoryCostField = document.getElementById(memoryCostId);
-    memoryCostField.innerText=cost;
-
-    const totalCost = totalCostCalculation();
-
-    const totalPriceField = document.getElementById('total-price');
-    totalPriceField.innerText=totalCost;
-
-    const totalPricePromoField =document.getElementById('promoCode-total');
-    totalPricePromoField.innerText=totalCost;
-
-}
-
-// function for storage
-function updateExtraStorage(storageCostId,cost) {
-    const storageCostField = document.getElementById(storageCostId);
-   
-    storageCostField.innerText=cost;
-
-    const totalCost = totalCostCalculation();
-
-    const totalPriceField = document.getElementById('total-price');
-    totalPriceField.innerText=totalCost;
-
-    const totalPricePromoField =document.getElementById('promoCode-total');
-    totalPricePromoField.innerText=totalCost;
-
-}
-
-// function for delivery charge
-function updateExtraStorage(deliveryCostId,cost) {
-    const deliveryCostField = document.getElementById(deliveryCostId);
-   
-    deliveryCostField.innerText=cost;
+// function for update cost 
+function updateCost(costId,cost) {
+    const costField = document.getElementById(costId);
+    costField.innerText=cost;
 
     const totalCost = totalCostCalculation();
 
@@ -67,13 +35,20 @@ function updatePromoTotal(totalCostId,promoCode) {
 // total cost calculation 
 function totalCostCalculation() {
     const bestPriceField = document.getElementById('best-price');
+    const bestPrice = parseFloat(bestPriceField.innerText);
+
     const extraMemoryField = document.getElementById('memory-cost');
+    const extraMemoryCost = parseFloat(extraMemoryField.innerText);
+
     const extraStorageField = document.getElementById('storage-cost');
-    const deliverChargeField = document.getElementById('delivery-charge');
+    const extraStorageCost = parseFloat(extraStorageField.innerText);
+
+    const deliveryChargeField = document.getElementById('delivery-charge');
+    const deliveryCost = parseFloat(deliveryChargeField.innerText);
    
 
     // total price 
-    const totalCost = parseFloat(bestPriceField.innerText)+parseFloat(extraMemoryField.innerText)+parseFloat(extraStorageField.innerText)+parseFloat(deliverChargeField.innerText);
+    const totalCost = bestPrice+extraMemoryCost+extraStorageCost+deliveryCost;
     return totalCost;
 
     
@@ -82,39 +57,39 @@ function totalCostCalculation() {
 // for memory 
 // for 8gb extra memory
 document.getElementById('memory-8gb').addEventListener('click',function(){
-    updateExtraMeomry('memory-cost',0);
+    updateCost('memory-cost',0);
 });
 
 // for 16gb extra memory
 document.getElementById('memory-16gb').addEventListener('click',function(){
-    updateExtraMeomry('memory-cost',180);
+    updateCost('memory-cost',180);
 });
 
 // for storage 
 // for 256gb extra storage
 document.getElementById('storage-256gb').addEventListener('click',function(){
-    updateExtraStorage('storage-cost',0);
+    updateCost('storage-cost',0);
 });
 // for 256gb extra storage
 document.getElementById('storage-512gb').addEventListener('click',function(){
-    updateExtraStorage('storage-cost',100);
+    updateCost('storage-cost',100);
 });
 // for 256gb extra storage
 document.getElementById('storage-1tb').addEventListener('click',function(){
-    updateExtraStorage('storage-cost',180);
+    updateCost('storage-cost',180);
 });
 
 
 // for delivery charge 
 // for free delivery
 document.getElementById('free-delivery').addEventListener('click',function(){
-    updateExtraMeomry('delivery-charge',0);
+    updateCost('delivery-charge',0);
    
 });
 
 // for delivery cost
 document.getElementById('charged-delivery').addEventListener('click',function(){
-    updateExtraMeomry('delivery-charge',20);
+    updateCost('delivery-charge',20);
    
 });
 
